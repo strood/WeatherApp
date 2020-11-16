288ece1e1fe1e59ad41f3655c0f9553e28783f37
strood <stroodz19@gmail.com>
2020-11-15 21:11:45 -0700

    refactor context into searchForm

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,19 +1,73 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useGlobalContext } from '../context';
+import { key } from '../config';
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
 
 export default function SearchForm() {
-  const { setSearchTerm, searchError, loadingSearch } = useGlobalContext();
+  const {
+    setSavedLocations,
+    savedLocations,
+    setLoadedLocations,
+    loadedLocations,
+  } = useGlobalContext();
+
+  const [searchError, setSearchError] = useState(false);
+  const [loadingSearch, setLoadingSearch] = useState(false);
 
   const searchValue = useRef('');
 
+  const fetchLocation = async () => {
+    setLoadingSearch(true);
+    console.log(searchValue.current.value);
+    try {
+      const response = await fetch(`${url}${searchValue.current.value}${key}`, {
+        code: 'cors',
+      });
+      const respData = await response.json();
+      console.log(respData);
+      if (respData.cod === '404') {
+        console.log(`Invalid Search: ${respData}`);
+        setSearchError('Invalid');
+        setLoadingSearch(false);
+      }
+      console.log(respData.id);
+
+      if (savedLocations.includes(String(respData.id))) {
+        respData.cod = 400;
+        setSearchError('Duplicate');
+        setLoadingSearch(false);
+      }
+      if (respData.cod === 200) {
+        console.log(`Valid Search^`);
+        console.log(loadedLocations);
+        console.log(savedLocations);
+        console.log(respData);
+        setLoadedLocations([...loadedLocations, respData]);
+        setSavedLocations([...savedLocations, String(respData.id)]);
+      }
+      setLoadingSearch(false);
+    } catch (error) {
+      console.log('Search Error');
+      console.log(error);
+      setSearchError('Error');
+      setLoadingSearch(false);
+    }
+  };
+
+  // useEffect(() => {
+  //   fetchLocation();
+  // }, [searchTerm]);
+
   useEffect(() => {
     searchValue.current.focus();
-  }, []);
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setSearchError(false);
     if (searchValue.current.value.length > 1) {
-      setSearchTerm(searchValue.current.value);
+      fetchLocation();
       searchValue.current.value = '';
     }
   };
--- src/context.js
+++ src/context.js
@@ -6,7 +6,6 @@ import { key } from './config';
 
 const AppContext = React.createContext();
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const groupUrl = 'https://api.openweathermap.org/data/2.5/group?id=';
 
 const getLocalLocations = () => {
@@ -19,11 +18,8 @@ const getLocalLocations = () => {
 };
 
 const AppProvider = ({ children }) => {
-  const [searchTerm, setSearchTerm] = useState('');
   const [loadingLocal, setLoadingLocal] = useState(true);
-  const [loadingSearch, setLoadingSearch] = useState(false);
   const [loadingError, setLoadingError] = useState(false);
-  const [searchError, setSearchError] = useState(false);
   // const [savedLocations, setSavedLocations] = useState(getLocalLocations());
   const [savedLocations, setSavedLocations] = useState(['5913490', '5419384']);
   const [loadedLocations, setLoadedLocations] = useState([]);
@@ -56,83 +52,16 @@ const AppProvider = ({ children }) => {
   useEffect(() => {
     fetchSavedLocations();
   }, []);
-  // const fetchSavedLocations = useCallback(async () => {
-  //   setLoadingLocal(true);
-  //   try {
-  //     console.log(`${groupUrl}${savedLocations.toString()}${key}`);
-  //     const response = await fetch(
-  //       `${groupUrl}${savedLocations.toString()}${key}`,
-  //       {
-  //         mode: 'cors',
-  //       }
-  //     );
-  //     const respData = await response.json();
-  //     setLoadedLocations(respData.list);
-  //     setLoadingLocal(false);
-  //   } catch (error) {
-  //     console.log(`Load Error: ${error}`);
-  //     setLoadingError(true);
-  //     setLoadingLocal(false);
-  //   }
-  // }, [savedLocations]);
-
-  // // Initial page load get saved locations.
-  // useEffect(() => {
-  //   fetchSavedLocations();
-  // }, [fetchSavedLocations]);
-
-  // Grab location on search, add to saved if valid
-  const fetchLocation = useCallback(async () => {
-    setLoadingSearch(true);
-    try {
-      const response = await fetch(`${url}${searchTerm}${key}`, {
-        code: 'cors',
-      });
-      const respData = await response.json();
-      console.log(respData);
-      if (respData.cod === '404') {
-        console.log(`Invalid Search: ${respData}`);
-        setSearchError('Invalid');
-        setLoadingSearch(false);
-      }
-      console.log(respData.id);
-      console.log(savedLocations);
-      if (savedLocations.includes(String(respData.id))) {
-        respData.cod = 400;
-        setSearchError('Duplicate');
-        setLoadingSearch(false);
-      }
-      if (respData.cod === 200) {
-        console.log(`Valid Search^`);
-
-        console.log(loadedLocations);
-        setLoadedLocations([...loadedLocations, respData]);
-        // setSavedLocations([...savedLocations, )
-      }
-      setLoadingSearch(false);
-    } catch (error) {
-      console.log('Search Error');
-      console.log(error);
-      setSearchError('Error');
-      setLoadingSearch(false);
-    }
-  }, [searchTerm]);
-
-  useEffect(() => {
-    fetchLocation();
-  }, [searchTerm, fetchLocation]);
 
   return (
     <AppContext.Provider
       value={{
         loadedLocations,
+        setLoadedLocations,
         savedLocations,
+        setSavedLocations,
         loadingLocal,
         loadingError,
-        searchError,
-        loadingSearch,
-        setSearchTerm,
-        setLoadingSearch,
       }}
     >
       {children}
